0c40ce7d32aa534ce8b34538498dc7688f965149
LIU Wei <liuwei@nbu.edu.cn>
2018-03-13 15:11:10 +0800

    add angular & react project

---
+++ WebAPI/Controllers/ToDoController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]/[action]")]
+    public class ToDoController : Controller
+    {
+        private readonly TodoContext _context;
+
+        public ToDoController(TodoContext todoContext)
+        {
+            _context = todoContext;
+            if (!_context.TodoItems.Any())
+            {
+                _context.TodoItems.Add(new TodoItem() {Name = "张三"});
+                _context.SaveChanges();
+            }
+        }
+        // GET: api/ToDo
+        [HttpGet]
+        public IEnumerable<TodoItem> Get()
+        {
+            return _context.TodoItems.ToList();
+        }
+
+        // GET: api/ToDo/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+        
+        // POST: api/ToDo
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+        
+        // PUT: api/ToDo/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+        
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -4,10 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using WebAPI;
+
 
 namespace LearningAspNetCore
 {
@@ -23,6 +26,7 @@ namespace LearningAspNetCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<TodoContext>(cob => cob.UseInMemoryDatabase("ToDoList"));
             services.AddMvc();
         }
 
